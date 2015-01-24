811a67c7025a938652f6877c5a0ae866467f39e2
John Bubriski <bubriski@gmail.com>
2015-01-24 15:53:57 -0500

    Uses iTween for arrow movemnt, fixes small cloud layering issue

--- Assets/Scripts/BounceMailboxArrow.cs
+++ Assets/Scripts/BounceMailboxArrow.cs
@@ -4,35 +4,35 @@ using System.Collections;
 public class BounceMailboxArrow : MonoBehaviour
 {
     public float MovementDistance = 3f;
-    public float MovementSpeed = 3f;
+    public float MovementTime = 3f;
 
     private float minY;
     private float maxY;
 
     private int direction = 1;
 
-    // Use this for initialization
     void Start()
     {
         maxY = transform.position.y + (MovementDistance / 2);
         minY = transform.position.y - (MovementDistance / 2);
+        
+		iTween.MoveTo(gameObject,iTween.Hash("y", minY, "time", MovementTime / 2, "easetype", "easeinQuad", "oncomplete", "MoveUp"));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (transform.position.y >= maxY)
-        {
-            direction = -1;
-        }
-
-        if (transform.position.y <= minY)
-        {
-            direction = 1;
-        }
-
-        transform.position = new Vector3(transform.position.x, transform.position.y + (MovementSpeed * Time.deltaTime * direction), transform.position.z);
-    }
+    
+	}
+	
+	void MoveUp()
+	{
+		iTween.MoveTo(gameObject,iTween.Hash("y", maxY, "time", MovementTime, "easetype", "easeOutQuad", "oncomplete", "MoveDown"));
+	}
+	
+	void MoveDown()
+	{
+		iTween.MoveTo(gameObject,iTween.Hash("y", minY, "time", MovementTime, "easetype", "easeinQuad", "oncomplete", "MoveUp"));
+	}
 
     void OnDrawGizmos()
     {
