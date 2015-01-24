1ebb5fb8fb5982f31bccc0d73cc9490e73dbd92c
Jared Barboza <jared.m.barboza@gmail.com>
2015-01-24 01:11:08 -0500

    bird flies off screen when hitting building

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : BaseBehavior
 	
 	public int ObstaclesPassed = 0;
 	public float JumpSpeed = 3.5F;
+
+    private int RPM = 90;
+    public bool IsDead = false;
 	
 	public PlayerType PlayerType
 	{
@@ -31,27 +34,43 @@ public class PlayerController : BaseBehavior
 
     void Update()
 	{
-		if (IsMobile() && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
-		{
-			rigidbody2D.velocity = Vector2.up * JumpSpeed;
-			
-			audio.Play();
-		}
-		else if (Input.GetButtonDown("Jump"))
-		{
-			rigidbody2D.velocity = Vector2.up * JumpSpeed;
-			
-			audio.Play();
+        if (!IsDead)
+        {
+            if (IsMobile() && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+            {
+                rigidbody2D.velocity = Vector2.up * JumpSpeed;
+
+                audio.Play();
+            }
+            else if (Input.GetButtonDown("Jump"))
+            {
+                rigidbody2D.velocity = Vector2.up * JumpSpeed;
+
+                audio.Play();
+            }
+
+            transform.Translate(Vector2.right * Time.deltaTime);
+
+            _particleSystem.enableEmission = rigidbody2D.velocity.y > 0;
         }
-        
-        transform.Translate(Vector2.right * Time.deltaTime);
-        
-        _particleSystem.enableEmission = rigidbody2D.velocity.y > 0;
+        else
+        {
+            // rotate the bird
+            transform.Rotate(0, 0, RPM * Time.deltaTime, Space.Self);
+
+            // bring the bird into the foreground
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+        }
     }
     
     public void OnDeath()
     {
 		AddHighScore(ObstaclesPassed);
+
+        IsDead = true;
+        rigidbody2D.velocity = Vector2.up * JumpSpeed * 2;
+
+        var foldDeath = gameObject.AddComponent<DieWhenBelowTheFold>();
     }
 
 	private void AddHighScore(int score)
